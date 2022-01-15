5984455b0f06498a5c95df1d485442bc18218458
Olga Morozova <morozova31@gmail.com>
2022-01-15 16:12:28 +0300

    отправка письма победителю аукциона

--- getwinner.php
+++ getwinner.php
@@ -1,37 +1,34 @@
 <?php
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
+require_once 'vendor/autoload.php';
 
-require_once('vendor/autoload.php');
+$dsn = 'smtp://olga@ladobor.ru:7M4j6I9k@mail.hosting.reg.ru:25';
+$transport = Transport::fromDsn($dsn);
 
-$transport = new Swift_SmtpTransport("phpdemo.ru", 25);
-$transport->setUsername("keks@phpdemo.ru");
-$transport->setPassword("htmlacademy");
-
-$mailer = new Swift_Mailer($transport);
-
-
+$user_winner = [];
+$connection = database_get_connection();
 $sql = "SELECT u.`email`, u.`username`, l.`id`, l.`heading` 
 FROM bet b JOIN lot l ON l.`id` = b.`bet_lot_id` 
 JOIN users u ON b.`bet_user_id` = u.`id` WHERE l.`finish` <= NOW()
-ORDER BY b.`date` DESC LIMIT 1;";
+ORDER BY b.`date` DESC LIMIT 1";
 
 $res = mysqli_query($connection, $sql);
 
 if ($res && mysqli_num_rows($res)) {
-    $winner = mysqli_fetch_all($res, MYSQLI_ASSOC);
-
+    $user_winner = mysqli_fetch_all($res, MYSQLI_ASSOC);
 }
 
-    $message = new Swift_Message();
-        $message->setSubject('Поздравляем с победой');
-        $message->setFrom(['keks@phpdemo.ru' => 'Yeticave']);
-        $message->setBcc($winner['email']);
-        $msg_content = include_template('email.php', ['winner' => $winner]);
-        $message->setBody($msg_content, 'text/html');
-        $result = $mailer->send($message);
-        if ($result) {
-            print("Рассылка успешно отправлена");
-        }
-        else {
-            print("Не удалось отправить рассылку");
-        }
+foreach ($user_winner as $user) {
 
+$message = new Email();
+$message->to($user['email']);
+$message->from('olga@ladobor.ru');
+$message->subject('Ваша ставка победила');
+$msg_content = include_template('email.php', ['user' => $user]);
+$message->html($msg_content, 'text/html');
+
+$mailer = new Mailer($transport);
+$mailer->send($message);
+}
--- templates/email.php
+++ templates/email.php
@@ -1,6 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
 <h1>Поздравляем с победой</h1>
-<p>Здравствуйте, <?=htmlspecialchars($winner['username']); ?></p>
-<p>Ваша ставка для лота <a href="http://169518-yeticave-12/lot.php?id=<?=htmlspecialchars($winner['id']); ?>"><?=htmlspecialchars($winner['heading']); ?></a> победила.</p>
-<p>Перейдите по ссылке <a href="http://169518-yeticave-12/my-bets.php">мои ставки</a>,
+<p>Здравствуйте, <?=htmlspecialchars($user['username']); ?>!</p>
+<p>Ваша ставка для лота <a href="http://localhost/169518-yeticave-12/lot.php?id=<?=htmlspecialchars($user['id']); ?>"><?=htmlspecialchars($user['heading']); ?></a> победила.</p>
+<p>Перейдите по ссылке <a href="http://localhost/169518-yeticave-12/my-bets.php">мои ставки</a>,
 чтобы связаться с автором объявления</p>
 <small>Интернет-Аукцион "YetiCave"</small>
+</body>
+</html>
+
